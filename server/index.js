959ebeef68533c8d9e1a50d778746c7a266c8cd3
Maor <Maor@MAOR-PC>
2022-01-22 19:32:05 +0200

    fix screen collection- save and update data

--- server/index.js
+++ server/index.js
@@ -16,8 +16,16 @@ const server = express();
 */
 server.get('/', (req, res) => {
 
-    const screenId = Number(req.query.id) % SCREEN_NUMBER;
+    const screenId = Number(req.query.id);
     print(`New connection from screen ID=${screenId}`);
+
+    ScreenModel.find({_id: screenId},function(err,result){
+
+        if(result.length==0){//if this screen id is not in the database
+            var screen = new ScreenModel({ _id: screenId, lastConnection: null});
+            screen.save();
+        }
+    });
 
     // Sending html page to the client
     const website = path.join(__dirname, "../client/index.html");
@@ -31,25 +39,35 @@ server.get('/', (req, res) => {
     The query parameter: id, represent the screen ID.
 */
 server.get('/advertisment', async (req, res) => {
-    const screenId = Number(req.query.id) % SCREEN_NUMBER;
+    const screenId = Number(req.query.id);
 
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+' '+time;
-    print(today);
+    var date = new Date();
+    var isoDateTime = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString();
 
-   // **TODO** -Maor
-    //var screen = new ScreenModel({ screenId: 1, lastConnection:today });
-	//await screen.save();
+    await ScreenModel.updateOne({ _id: screenId }, { $set: { lastConnection: isoDateTime } });
 
     print(`Receive request from screen ID=${screenId} for advertisment data`);
 
-    const screenAdvertisment = await fetchAdvertismentByScreenId(screenId);
+    const screenAdvertisment = await fetchAdvertismentByScreenId(screenId% SCREEN_NUMBER);
     print(`send ${screenAdvertisment.length} advertisment to the screen ID=${screenId}`);
 
     return res.json(screenAdvertisment);
 });
+
+server.get('/maor', (req, res) => {
+
+    var date = new Date();
+    var isoDateTime = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString();
+
+    /*ScreenModel.find({lastConnection: { $lte:isoDateTime},function(err,result){
+        res.write(result);
+    }})*/
+    ScreenModel.find({},function(err,result){
+        res.write(result);
+    })
+    res.write("jhtyjtuyrfuyrf");
+    res.end();
+})
 
 /**
     Define the public directory of the system.
